02b51a098117513d5ddd4b7e6f42f9e3e75617d8
root <nagaraja.hm.0001@gmail.com>
2022-01-07 20:16:39 +0100

    Update: submodule Csm

--- LATEST/Csm.cpp
+++ LATEST/Csm.cpp
@@ -5,3 +5,111 @@ class_Csm Csm;
 void class_Csm::InitFunction(void){
 }
 
+void class_Csm::GetVersionInfo(void){
+}
+
+void class_Csm::Hash(void){
+}
+
+void class_Csm::MacGenerate(void){
+}
+
+void class_Csm::MacVerify(void){
+}
+
+void class_Csm::Encrypt(void){
+}
+
+void class_Csm::Decrypt(void){
+}
+
+void class_Csm::AEADEncrypt(void){
+}
+
+void class_Csm::AEADDecrypt(void){
+}
+
+void class_Csm::SignatureGenerate(void){
+}
+
+void class_Csm::SignatureVerify(void){
+}
+
+void class_Csm::RandomGenerate(void){
+}
+
+void class_Csm::KeyElementSet(void){
+}
+
+void class_Csm::KeySetValid(void){
+}
+
+void class_Csm::KeySetInvalid(void){
+}
+
+void class_Csm::KeyGetStatus(void){
+}
+
+void class_Csm::KeyElementGet(void){
+}
+
+void class_Csm::KeyElementCopy(void){
+}
+
+void class_Csm::KeyCopy(void){
+}
+
+void class_Csm::KeyElementCopyPartial(void){
+}
+
+void class_Csm::RandomSeed(void){
+}
+
+void class_Csm::KeyGenerate(void){
+}
+
+void class_Csm::KeyDerive(void){
+}
+
+void class_Csm::KeyExchangeCalcPubValue(void){
+}
+
+void class_Csm::KeyExchangeCalcSecret(void){
+}
+
+void class_Csm::JobKeySetValid(void){
+}
+
+void class_Csm::JobKeySetInvalid(void){
+}
+
+void class_Csm::JobRandomSeed(void){
+}
+
+void class_Csm::JobKeyGenerate(void){
+}
+
+void class_Csm::JobKeyDerive(void){
+}
+
+void class_Csm::JobKeyExchangeCalcPubValue(void){
+}
+
+void class_Csm::JobKeyExchangeCalcSecret(void){
+}
+
+void class_Csm::SaveContextJob(void){
+}
+
+void class_Csm::RestoreContextJob(void){
+}
+
+void class_Csm::CancelJob(void){
+}
+
+void class_Csm::CbNotification(void){
+}
+
+void class_Csm::MainFunction(void){
+}
+
--- LATEST/Csm.h
+++ LATEST/Csm.h
@@ -2,7 +2,43 @@
 
 class class_Csm{
    public:
-      void InitFunction(void);
+      void InitFunction               (void);
+      void GetVersionInfo             (void);
+      void Hash                       (void);
+      void MacGenerate                (void);
+      void MacVerify                  (void);
+      void Encrypt                    (void);
+      void Decrypt                    (void);
+      void AEADEncrypt                (void);
+      void AEADDecrypt                (void);
+      void SignatureGenerate          (void);
+      void SignatureVerify            (void);
+      void RandomGenerate             (void);
+      void KeyElementSet              (void);
+      void KeySetValid                (void);
+      void KeySetInvalid              (void);
+      void KeyGetStatus               (void);
+      void KeyElementGet              (void);
+      void KeyElementCopy             (void);
+      void KeyCopy                    (void);
+      void KeyElementCopyPartial      (void);
+      void RandomSeed                 (void);
+      void KeyGenerate                (void);
+      void KeyDerive                  (void);
+      void KeyExchangeCalcPubValue    (void);
+      void KeyExchangeCalcSecret      (void);
+      void JobKeySetValid             (void);
+      void JobKeySetInvalid           (void);
+      void JobRandomSeed              (void);
+      void JobKeyGenerate             (void);
+      void JobKeyDerive               (void);
+      void JobKeyExchangeCalcPubValue (void);
+      void JobKeyExchangeCalcSecret   (void);
+      void SaveContextJob             (void);
+      void RestoreContextJob          (void);
+      void CancelJob                  (void);
+      void CbNotification             (void);
+      void MainFunction               (void);
 };
 
 extern class_Csm Csm;
