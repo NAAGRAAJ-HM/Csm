95ad44e147f6618dbfab5048acacbacceeb060b7
root <nagaraja.hm.0001@gmail.com>
2022-01-12 09:25:59 +0100

    Update: submodule Csm

--- LATEST/Csm.cpp
+++ LATEST/Csm.cpp
@@ -2,114 +2,114 @@
 
 class_Csm Csm;
 
-void class_Csm::InitFunction(void){
+FUNC(void, CSM_CODE) class_Csm::InitFunction(void){
 }
 
-void class_Csm::GetVersionInfo(void){
+FUNC(void, CSM_CODE) class_Csm::GetVersionInfo(void){
 }
 
-void class_Csm::Hash(void){
+FUNC(void, CSM_CODE) class_Csm::Hash(void){
 }
 
-void class_Csm::MacGenerate(void){
+FUNC(void, CSM_CODE) class_Csm::MacGenerate(void){
 }
 
-void class_Csm::MacVerify(void){
+FUNC(void, CSM_CODE) class_Csm::MacVerify(void){
 }
 
-void class_Csm::Encrypt(void){
+FUNC(void, CSM_CODE) class_Csm::Encrypt(void){
 }
 
-void class_Csm::Decrypt(void){
+FUNC(void, CSM_CODE) class_Csm::Decrypt(void){
 }
 
-void class_Csm::AEADEncrypt(void){
+FUNC(void, CSM_CODE) class_Csm::AEADEncrypt(void){
 }
 
-void class_Csm::AEADDecrypt(void){
+FUNC(void, CSM_CODE) class_Csm::AEADDecrypt(void){
 }
 
-void class_Csm::SignatureGenerate(void){
+FUNC(void, CSM_CODE) class_Csm::SignatureGenerate(void){
 }
 
-void class_Csm::SignatureVerify(void){
+FUNC(void, CSM_CODE) class_Csm::SignatureVerify(void){
 }
 
-void class_Csm::RandomGenerate(void){
+FUNC(void, CSM_CODE) class_Csm::RandomGenerate(void){
 }
 
-void class_Csm::KeyElementSet(void){
+FUNC(void, CSM_CODE) class_Csm::KeyElementSet(void){
 }
 
-void class_Csm::KeySetValid(void){
+FUNC(void, CSM_CODE) class_Csm::KeySetValid(void){
 }
 
-void class_Csm::KeySetInvalid(void){
+FUNC(void, CSM_CODE) class_Csm::KeySetInvalid(void){
 }
 
-void class_Csm::KeyGetStatus(void){
+FUNC(void, CSM_CODE) class_Csm::KeyGetStatus(void){
 }
 
-void class_Csm::KeyElementGet(void){
+FUNC(void, CSM_CODE) class_Csm::KeyElementGet(void){
 }
 
-void class_Csm::KeyElementCopy(void){
+FUNC(void, CSM_CODE) class_Csm::KeyElementCopy(void){
 }
 
-void class_Csm::KeyCopy(void){
+FUNC(void, CSM_CODE) class_Csm::KeyCopy(void){
 }
 
-void class_Csm::KeyElementCopyPartial(void){
+FUNC(void, CSM_CODE) class_Csm::KeyElementCopyPartial(void){
 }
 
-void class_Csm::RandomSeed(void){
+FUNC(void, CSM_CODE) class_Csm::RandomSeed(void){
 }
 
-void class_Csm::KeyGenerate(void){
+FUNC(void, CSM_CODE) class_Csm::KeyGenerate(void){
 }
 
-void class_Csm::KeyDerive(void){
+FUNC(void, CSM_CODE) class_Csm::KeyDerive(void){
 }
 
-void class_Csm::KeyExchangeCalcPubValue(void){
+FUNC(void, CSM_CODE) class_Csm::KeyExchangeCalcPubValue(void){
 }
 
-void class_Csm::KeyExchangeCalcSecret(void){
+FUNC(void, CSM_CODE) class_Csm::KeyExchangeCalcSecret(void){
 }
 
-void class_Csm::JobKeySetValid(void){
+FUNC(void, CSM_CODE) class_Csm::JobKeySetValid(void){
 }
 
-void class_Csm::JobKeySetInvalid(void){
+FUNC(void, CSM_CODE) class_Csm::JobKeySetInvalid(void){
 }
 
-void class_Csm::JobRandomSeed(void){
+FUNC(void, CSM_CODE) class_Csm::JobRandomSeed(void){
 }
 
-void class_Csm::JobKeyGenerate(void){
+FUNC(void, CSM_CODE) class_Csm::JobKeyGenerate(void){
 }
 
-void class_Csm::JobKeyDerive(void){
+FUNC(void, CSM_CODE) class_Csm::JobKeyDerive(void){
 }
 
-void class_Csm::JobKeyExchangeCalcPubValue(void){
+FUNC(void, CSM_CODE) class_Csm::JobKeyExchangeCalcPubValue(void){
 }
 
-void class_Csm::JobKeyExchangeCalcSecret(void){
+FUNC(void, CSM_CODE) class_Csm::JobKeyExchangeCalcSecret(void){
 }
 
-void class_Csm::SaveContextJob(void){
+FUNC(void, CSM_CODE) class_Csm::SaveContextJob(void){
 }
 
-void class_Csm::RestoreContextJob(void){
+FUNC(void, CSM_CODE) class_Csm::RestoreContextJob(void){
 }
 
-void class_Csm::CancelJob(void){
+FUNC(void, CSM_CODE) class_Csm::CancelJob(void){
 }
 
-void class_Csm::CbNotification(void){
+FUNC(void, CSM_CODE) class_Csm::CbNotification(void){
 }
 
-void class_Csm::MainFunction(void){
+FUNC(void, CSM_CODE) class_Csm::MainFunction(void){
 }
 
--- LATEST/Csm.h
+++ LATEST/Csm.h
@@ -1,46 +1,47 @@
 #pragma once
 
 #include "Std_Types.h"
+#include "Compiler_Cfg_Csm.h"
 
 class class_Csm{
    public:
-      void InitFunction               (void);
-      void GetVersionInfo             (void);
-      void Hash                       (void);
-      void MacGenerate                (void);
-      void MacVerify                  (void);
-      void Encrypt                    (void);
-      void Decrypt                    (void);
-      void AEADEncrypt                (void);
-      void AEADDecrypt                (void);
-      void SignatureGenerate          (void);
-      void SignatureVerify            (void);
-      void RandomGenerate             (void);
-      void KeyElementSet              (void);
-      void KeySetValid                (void);
-      void KeySetInvalid              (void);
-      void KeyGetStatus               (void);
-      void KeyElementGet              (void);
-      void KeyElementCopy             (void);
-      void KeyCopy                    (void);
-      void KeyElementCopyPartial      (void);
-      void RandomSeed                 (void);
-      void KeyGenerate                (void);
-      void KeyDerive                  (void);
-      void KeyExchangeCalcPubValue    (void);
-      void KeyExchangeCalcSecret      (void);
-      void JobKeySetValid             (void);
-      void JobKeySetInvalid           (void);
-      void JobRandomSeed              (void);
-      void JobKeyGenerate             (void);
-      void JobKeyDerive               (void);
-      void JobKeyExchangeCalcPubValue (void);
-      void JobKeyExchangeCalcSecret   (void);
-      void SaveContextJob             (void);
-      void RestoreContextJob          (void);
-      void CancelJob                  (void);
-      void CbNotification             (void);
-      void MainFunction               (void);
+      FUNC(void, CSM_CODE) InitFunction               (void);
+      FUNC(void, CSM_CODE) GetVersionInfo             (void);
+      FUNC(void, CSM_CODE) Hash                       (void);
+      FUNC(void, CSM_CODE) MacGenerate                (void);
+      FUNC(void, CSM_CODE) MacVerify                  (void);
+      FUNC(void, CSM_CODE) Encrypt                    (void);
+      FUNC(void, CSM_CODE) Decrypt                    (void);
+      FUNC(void, CSM_CODE) AEADEncrypt                (void);
+      FUNC(void, CSM_CODE) AEADDecrypt                (void);
+      FUNC(void, CSM_CODE) SignatureGenerate          (void);
+      FUNC(void, CSM_CODE) SignatureVerify            (void);
+      FUNC(void, CSM_CODE) RandomGenerate             (void);
+      FUNC(void, CSM_CODE) KeyElementSet              (void);
+      FUNC(void, CSM_CODE) KeySetValid                (void);
+      FUNC(void, CSM_CODE) KeySetInvalid              (void);
+      FUNC(void, CSM_CODE) KeyGetStatus               (void);
+      FUNC(void, CSM_CODE) KeyElementGet              (void);
+      FUNC(void, CSM_CODE) KeyElementCopy             (void);
+      FUNC(void, CSM_CODE) KeyCopy                    (void);
+      FUNC(void, CSM_CODE) KeyElementCopyPartial      (void);
+      FUNC(void, CSM_CODE) RandomSeed                 (void);
+      FUNC(void, CSM_CODE) KeyGenerate                (void);
+      FUNC(void, CSM_CODE) KeyDerive                  (void);
+      FUNC(void, CSM_CODE) KeyExchangeCalcPubValue    (void);
+      FUNC(void, CSM_CODE) KeyExchangeCalcSecret      (void);
+      FUNC(void, CSM_CODE) JobKeySetValid             (void);
+      FUNC(void, CSM_CODE) JobKeySetInvalid           (void);
+      FUNC(void, CSM_CODE) JobRandomSeed              (void);
+      FUNC(void, CSM_CODE) JobKeyGenerate             (void);
+      FUNC(void, CSM_CODE) JobKeyDerive               (void);
+      FUNC(void, CSM_CODE) JobKeyExchangeCalcPubValue (void);
+      FUNC(void, CSM_CODE) JobKeyExchangeCalcSecret   (void);
+      FUNC(void, CSM_CODE) SaveContextJob             (void);
+      FUNC(void, CSM_CODE) RestoreContextJob          (void);
+      FUNC(void, CSM_CODE) CancelJob                  (void);
+      FUNC(void, CSM_CODE) CbNotification             (void);
+      FUNC(void, CSM_CODE) MainFunction               (void);
 };
 
 extern class_Csm Csm;
