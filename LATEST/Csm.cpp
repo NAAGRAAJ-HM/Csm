5eab8488bf7ca21fee8a7ee85f391236bf0b2080
root <nagaraja.hm.0001@gmail.com>
2022-02-10 12:40:07 +0100

    Update: submodule Csm

--- LATEST/Csm.cpp
+++ LATEST/Csm.cpp
@@ -6,133 +6,148 @@
 /*****************************************************/
 /* #INCLUDES                                         */
 /*****************************************************/
-#include "Csm.h"
+#include "module.h"
 
-#include "Csm_EcuM.h"
-#include "Csm_SchM.h"
+#include "Csm_Unused.h"
+
+/*****************************************************/
+/* #DEFINES                                          */
+/*****************************************************/
+
+/*****************************************************/
+/* MACROS                                            */
+/*****************************************************/
+
+/*****************************************************/
+/* TYPEDEFS                                          */
+/*****************************************************/
+class module_Csm : public class_module{
+   public:
+      FUNC(void, CSM_CODE) InitFunction   (void);
+      FUNC(void, CSM_CODE) DeInitFunction (void);
+      FUNC(void, CSM_CODE) MainFunction   (void);
+};
 
 /*****************************************************/
 /* OBJECTS                                           */
 /*****************************************************/
-interface_Csm_EcuM_Init Csm_EcuM_Init;
-interface_Csm_SchM_Main Csm_SchM_Main;
-class_Csm Csm;
+module_Csm Csm;
 
-interface_EcuM_Init_Client *EcuM_Init_Client_ptr_Csm = &Csm_EcuM_Init;
-interface_SchM_Main_Client *SchM_Main_Client_ptr_Csm = &Csm_SchM_Main;
+interface_EcuM_Client *EcuM_Client_ptr_Csm = &Csm;
+interface_SchM_Client *SchM_Client_ptr_Csm = &Csm;
 
 /*****************************************************/
 /* FUNCTIONS                                         */
 /*****************************************************/
-FUNC(void, CSM_CODE) interface_Csm_EcuM_Init::InitFunction(void){
+FUNC(void, CSM_CODE) module_Csm::InitFunction(void){
 }
 
-FUNC(void, CSM_CODE) interface_Csm_SchM_Main::MainFunction(void){
+FUNC(void, CSM_CODE) module_Csm::MainFunction(void){
 }
 
-FUNC(void, CSM_CODE) class_Csm::GetVersionInfo(void){
+FUNC(void, CSM_CODE) class_Csm_Unused::GetVersionInfo(void){
 }
 
-FUNC(void, CSM_CODE) class_Csm::Hash(void){
+FUNC(void, CSM_CODE) class_Csm_Unused::Hash(void){
 }
 
-FUNC(void, CSM_CODE) class_Csm::MacGenerate(void){
+FUNC(void, CSM_CODE) class_Csm_Unused::MacGenerate(void){
 }
 
-FUNC(void, CSM_CODE) class_Csm::MacVerify(void){
+FUNC(void, CSM_CODE) class_Csm_Unused::MacVerify(void){
 }
 
-FUNC(void, CSM_CODE) class_Csm::Encrypt(void){
+FUNC(void, CSM_CODE) class_Csm_Unused::Encrypt(void){
 }
 
-FUNC(void, CSM_CODE) class_Csm::Decrypt(void){
+FUNC(void, CSM_CODE) class_Csm_Unused::Decrypt(void){
 }
 
-FUNC(void, CSM_CODE) class_Csm::AEADEncrypt(void){
+FUNC(void, CSM_CODE) class_Csm_Unused::AEADEncrypt(void){
 }
 
-FUNC(void, CSM_CODE) class_Csm::AEADDecrypt(void){
+FUNC(void, CSM_CODE) class_Csm_Unused::AEADDecrypt(void){
 }
 
-FUNC(void, CSM_CODE) class_Csm::SignatureGenerate(void){
+FUNC(void, CSM_CODE) class_Csm_Unused::SignatureGenerate(void){
 }
 
-FUNC(void, CSM_CODE) class_Csm::SignatureVerify(void){
+FUNC(void, CSM_CODE) class_Csm_Unused::SignatureVerify(void){
 }
 
-FUNC(void, CSM_CODE) class_Csm::RandomGenerate(void){
+FUNC(void, CSM_CODE) class_Csm_Unused::RandomGenerate(void){
 }
 
-FUNC(void, CSM_CODE) class_Csm::KeyElementSet(void){
+FUNC(void, CSM_CODE) class_Csm_Unused::KeyElementSet(void){
 }
 
-FUNC(void, CSM_CODE) class_Csm::KeySetValid(void){
+FUNC(void, CSM_CODE) class_Csm_Unused::KeySetValid(void){
 }
 
-FUNC(void, CSM_CODE) class_Csm::KeySetInvalid(void){
+FUNC(void, CSM_CODE) class_Csm_Unused::KeySetInvalid(void){
 }
 
-FUNC(void, CSM_CODE) class_Csm::KeyGetStatus(void){
+FUNC(void, CSM_CODE) class_Csm_Unused::KeyGetStatus(void){
 }
 
-FUNC(void, CSM_CODE) class_Csm::KeyElementGet(void){
+FUNC(void, CSM_CODE) class_Csm_Unused::KeyElementGet(void){
 }
 
-FUNC(void, CSM_CODE) class_Csm::KeyElementCopy(void){
+FUNC(void, CSM_CODE) class_Csm_Unused::KeyElementCopy(void){
 }
 
-FUNC(void, CSM_CODE) class_Csm::KeyCopy(void){
+FUNC(void, CSM_CODE) class_Csm_Unused::KeyCopy(void){
 }
 
-FUNC(void, CSM_CODE) class_Csm::KeyElementCopyPartial(void){
+FUNC(void, CSM_CODE) class_Csm_Unused::KeyElementCopyPartial(void){
 }
 
-FUNC(void, CSM_CODE) class_Csm::RandomSeed(void){
+FUNC(void, CSM_CODE) class_Csm_Unused::RandomSeed(void){
 }
 
-FUNC(void, CSM_CODE) class_Csm::KeyGenerate(void){
+FUNC(void, CSM_CODE) class_Csm_Unused::KeyGenerate(void){
 }
 
-FUNC(void, CSM_CODE) class_Csm::KeyDerive(void){
+FUNC(void, CSM_CODE) class_Csm_Unused::KeyDerive(void){
 }
 
-FUNC(void, CSM_CODE) class_Csm::KeyExchangeCalcPubValue(void){
+FUNC(void, CSM_CODE) class_Csm_Unused::KeyExchangeCalcPubValue(void){
 }
 
-FUNC(void, CSM_CODE) class_Csm::KeyExchangeCalcSecret(void){
+FUNC(void, CSM_CODE) class_Csm_Unused::KeyExchangeCalcSecret(void){
 }
 
-FUNC(void, CSM_CODE) class_Csm::JobKeySetValid(void){
+FUNC(void, CSM_CODE) class_Csm_Unused::JobKeySetValid(void){
 }
 
-FUNC(void, CSM_CODE) class_Csm::JobKeySetInvalid(void){
+FUNC(void, CSM_CODE) class_Csm_Unused::JobKeySetInvalid(void){
 }
 
-FUNC(void, CSM_CODE) class_Csm::JobRandomSeed(void){
+FUNC(void, CSM_CODE) class_Csm_Unused::JobRandomSeed(void){
 }
 
-FUNC(void, CSM_CODE) class_Csm::JobKeyGenerate(void){
+FUNC(void, CSM_CODE) class_Csm_Unused::JobKeyGenerate(void){
 }
 
-FUNC(void, CSM_CODE) class_Csm::JobKeyDerive(void){
+FUNC(void, CSM_CODE) class_Csm_Unused::JobKeyDerive(void){
 }
 
-FUNC(void, CSM_CODE) class_Csm::JobKeyExchangeCalcPubValue(void){
+FUNC(void, CSM_CODE) class_Csm_Unused::JobKeyExchangeCalcPubValue(void){
 }
 
-FUNC(void, CSM_CODE) class_Csm::JobKeyExchangeCalcSecret(void){
+FUNC(void, CSM_CODE) class_Csm_Unused::JobKeyExchangeCalcSecret(void){
 }
 
-FUNC(void, CSM_CODE) class_Csm::SaveContextJob(void){
+FUNC(void, CSM_CODE) class_Csm_Unused::SaveContextJob(void){
 }
 
-FUNC(void, CSM_CODE) class_Csm::RestoreContextJob(void){
+FUNC(void, CSM_CODE) class_Csm_Unused::RestoreContextJob(void){
 }
 
-FUNC(void, CSM_CODE) class_Csm::CancelJob(void){
+FUNC(void, CSM_CODE) class_Csm_Unused::CancelJob(void){
 }
 
-FUNC(void, CSM_CODE) class_Csm::CbNotification(void){
+FUNC(void, CSM_CODE) class_Csm_Unused::CbNotification(void){
 }
 
 /*****************************************************/
