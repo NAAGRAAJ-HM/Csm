c41372b413d64f956d67014f7df33a9d5ddfca6f
root <nagaraja.hm.0001@gmail.com>
2022-02-25 12:07:54 +0100

    Update: submodule Csm

--- LATEST/Csm_EcuM.h
+++ LATEST/Csm_EcuM.h
@@ -27,7 +27,8 @@ class interface_Csm_EcuM : public interface_EcuM_Client{
 /*****************************************************/
 /* FUNCTIONS                                         */
 /*****************************************************/
-      FUNC(void, CSM_CODE) InitFunction(void);
+      virtual FUNC(void, CSM_CODE) InitFunction   (void) = 0;
+      virtual FUNC(void, CSM_CODE) DeInitFunction (void) = 0;
 };
 
 /*****************************************************/
