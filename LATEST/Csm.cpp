962e47babee808cd1386671b994b3aa8ad3753e1
NAGARAJA HM <nagaraja.hm.0001@gmail.com>
2022-04-07 09:25:31 +0000

    Update: AUTOSAR basic software service module Csm

--- LATEST/Csm.cpp
+++ LATEST/Csm.cpp
@@ -101,8 +101,8 @@ FUNC(void, CSM_CODE) module_Csm::InitFunction(
             lptrCfg = lptrCfgModule;
          }
          else{
-// use PBcfg_CanIf as back-up configuration
-            lptrCfg = PBcfg_CanIf;
+// use PBcfgCanIf as back-up configuration
+            lptrCfg = &PBcfgCsm;
          }
       }
       IsInitDone = E_OK;
