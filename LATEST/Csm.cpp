abfc049dcc18d6c612aefe80c394c6bff9f65235
NAGARAJA HM <nagaraja.hm.0001@gmail.com>
2022-04-26 10:10:27 +0000

    Update: AUTOSAR basic software service module Csm

--- LATEST/Csm.cpp
+++ LATEST/Csm.cpp
@@ -31,8 +31,47 @@
 /******************************************************************************/
 /* TYPEDEFS                                                                   */
 /******************************************************************************/
+class class_Csm_Functionality{
+   public:
+      FUNC(void, CSM_CODE) Hash                       (void);
+      FUNC(void, CSM_CODE) MacGenerate                (void);
+      FUNC(void, CSM_CODE) MacVerify                  (void);
+      FUNC(void, CSM_CODE) Encrypt                    (void);
+      FUNC(void, CSM_CODE) Decrypt                    (void);
+      FUNC(void, CSM_CODE) AEADEncrypt                (void);
+      FUNC(void, CSM_CODE) AEADDecrypt                (void);
+      FUNC(void, CSM_CODE) SignatureGenerate          (void);
+      FUNC(void, CSM_CODE) SignatureVerify            (void);
+      FUNC(void, CSM_CODE) RandomGenerate             (void);
+      FUNC(void, CSM_CODE) KeyElementSet              (void);
+      FUNC(void, CSM_CODE) KeySetValid                (void);
+      FUNC(void, CSM_CODE) KeySetInvalid              (void);
+      FUNC(void, CSM_CODE) KeyGetStatus               (void);
+      FUNC(void, CSM_CODE) KeyElementGet              (void);
+      FUNC(void, CSM_CODE) KeyElementCopy             (void);
+      FUNC(void, CSM_CODE) KeyCopy                    (void);
+      FUNC(void, CSM_CODE) KeyElementCopyPartial      (void);
+      FUNC(void, CSM_CODE) RandomSeed                 (void);
+      FUNC(void, CSM_CODE) KeyGenerate                (void);
+      FUNC(void, CSM_CODE) KeyDerive                  (void);
+      FUNC(void, CSM_CODE) KeyExchangeCalcPubValue    (void);
+      FUNC(void, CSM_CODE) KeyExchangeCalcSecret      (void);
+      FUNC(void, CSM_CODE) JobKeySetValid             (void);
+      FUNC(void, CSM_CODE) JobKeySetInvalid           (void);
+      FUNC(void, CSM_CODE) JobRandomSeed              (void);
+      FUNC(void, CSM_CODE) JobKeyGenerate             (void);
+      FUNC(void, CSM_CODE) JobKeyDerive               (void);
+      FUNC(void, CSM_CODE) JobKeyExchangeCalcPubValue (void);
+      FUNC(void, CSM_CODE) JobKeyExchangeCalcSecret   (void);
+      FUNC(void, CSM_CODE) SaveContextJob             (void);
+      FUNC(void, CSM_CODE) RestoreContextJob          (void);
+      FUNC(void, CSM_CODE) CancelJob                  (void);
+      FUNC(void, CSM_CODE) CbNotification             (void);
+};
+
 class module_Csm:
       public abstract_module
+   ,  public class_Csm_Functionality
 {
    public:
       module_Csm(Std_TypeVersionInfo lVersionInfo) : abstract_module(lVersionInfo){
@@ -84,6 +123,10 @@ FUNC(void, CSM_CODE) module_Csm::InitFunction(
    if(E_OK == IsInitDone){
 #if(STD_ON == Csm_DevErrorDetect)
       Det_ReportError(
+      0 //TBD: IdModule
+   ,  0 //TBD: IdInstance
+   ,  0 //TBD: IdApi
+   ,  0 //TBD: IdError
       );
 #endif
    }
@@ -92,6 +135,10 @@ FUNC(void, CSM_CODE) module_Csm::InitFunction(
       if(NULL_PTR == lptrCfgModule){
 #if(STD_ON == Csm_DevErrorDetect)
          Det_ReportError(
+      0 //TBD: IdModule
+   ,  0 //TBD: IdInstance
+   ,  0 //TBD: IdApi
+   ,  0 //TBD: IdError
          );
 #endif
       }
@@ -116,6 +163,10 @@ FUNC(void, CSM_CODE) module_Csm::DeInitFunction(void){
    if(E_OK != IsInitDone){
 #if(STD_ON == Csm_DevErrorDetect)
       Det_ReportError(
+      0 //TBD: IdModule
+   ,  0 //TBD: IdInstance
+   ,  0 //TBD: IdApi
+   ,  0 //TBD: IdError
       );
 #endif
    }
@@ -132,6 +183,10 @@ FUNC(void, CSM_CODE) module_Csm::MainFunction(void){
    if(E_OK != IsInitDone){
 #if(STD_ON == Csm_DevErrorDetect)
       Det_ReportError(
+      0 //TBD: IdModule
+   ,  0 //TBD: IdInstance
+   ,  0 //TBD: IdApi
+   ,  0 //TBD: IdError
       );
 #endif
    }
@@ -142,144 +197,106 @@ FUNC(void, CSM_CODE) module_Csm::MainFunction(void){
 #endif
 }
 
-class class_Csm_Unused{
-   public:
-      FUNC(void, CSM_CODE) Hash                       (void);
-      FUNC(void, CSM_CODE) MacGenerate                (void);
-      FUNC(void, CSM_CODE) MacVerify                  (void);
-      FUNC(void, CSM_CODE) Encrypt                    (void);
-      FUNC(void, CSM_CODE) Decrypt                    (void);
-      FUNC(void, CSM_CODE) AEADEncrypt                (void);
-      FUNC(void, CSM_CODE) AEADDecrypt                (void);
-      FUNC(void, CSM_CODE) SignatureGenerate          (void);
-      FUNC(void, CSM_CODE) SignatureVerify            (void);
-      FUNC(void, CSM_CODE) RandomGenerate             (void);
-      FUNC(void, CSM_CODE) KeyElementSet              (void);
-      FUNC(void, CSM_CODE) KeySetValid                (void);
-      FUNC(void, CSM_CODE) KeySetInvalid              (void);
-      FUNC(void, CSM_CODE) KeyGetStatus               (void);
-      FUNC(void, CSM_CODE) KeyElementGet              (void);
-      FUNC(void, CSM_CODE) KeyElementCopy             (void);
-      FUNC(void, CSM_CODE) KeyCopy                    (void);
-      FUNC(void, CSM_CODE) KeyElementCopyPartial      (void);
-      FUNC(void, CSM_CODE) RandomSeed                 (void);
-      FUNC(void, CSM_CODE) KeyGenerate                (void);
-      FUNC(void, CSM_CODE) KeyDerive                  (void);
-      FUNC(void, CSM_CODE) KeyExchangeCalcPubValue    (void);
-      FUNC(void, CSM_CODE) KeyExchangeCalcSecret      (void);
-      FUNC(void, CSM_CODE) JobKeySetValid             (void);
-      FUNC(void, CSM_CODE) JobKeySetInvalid           (void);
-      FUNC(void, CSM_CODE) JobRandomSeed              (void);
-      FUNC(void, CSM_CODE) JobKeyGenerate             (void);
-      FUNC(void, CSM_CODE) JobKeyDerive               (void);
-      FUNC(void, CSM_CODE) JobKeyExchangeCalcPubValue (void);
-      FUNC(void, CSM_CODE) JobKeyExchangeCalcSecret   (void);
-      FUNC(void, CSM_CODE) SaveContextJob             (void);
-      FUNC(void, CSM_CODE) RestoreContextJob          (void);
-      FUNC(void, CSM_CODE) CancelJob                  (void);
-      FUNC(void, CSM_CODE) CbNotification             (void);
-};
-
-FUNC(void, CSM_CODE) class_Csm_Unused::Hash(void){
+FUNC(void, CSM_CODE) class_Csm_Functionality::Hash(void){
 }
 
-FUNC(void, CSM_CODE) class_Csm_Unused::MacGenerate(void){
+FUNC(void, CSM_CODE) class_Csm_Functionality::MacGenerate(void){
 }
 
-FUNC(void, CSM_CODE) class_Csm_Unused::MacVerify(void){
+FUNC(void, CSM_CODE) class_Csm_Functionality::MacVerify(void){
 }
 
-FUNC(void, CSM_CODE) class_Csm_Unused::Encrypt(void){
+FUNC(void, CSM_CODE) class_Csm_Functionality::Encrypt(void){
 }
 
-FUNC(void, CSM_CODE) class_Csm_Unused::Decrypt(void){
+FUNC(void, CSM_CODE) class_Csm_Functionality::Decrypt(void){
 }
 
-FUNC(void, CSM_CODE) class_Csm_Unused::AEADEncrypt(void){
+FUNC(void, CSM_CODE) class_Csm_Functionality::AEADEncrypt(void){
 }
 
-FUNC(void, CSM_CODE) class_Csm_Unused::AEADDecrypt(void){
+FUNC(void, CSM_CODE) class_Csm_Functionality::AEADDecrypt(void){
 }
 
-FUNC(void, CSM_CODE) class_Csm_Unused::SignatureGenerate(void){
+FUNC(void, CSM_CODE) class_Csm_Functionality::SignatureGenerate(void){
 }
 
-FUNC(void, CSM_CODE) class_Csm_Unused::SignatureVerify(void){
+FUNC(void, CSM_CODE) class_Csm_Functionality::SignatureVerify(void){
 }
 
-FUNC(void, CSM_CODE) class_Csm_Unused::RandomGenerate(void){
+FUNC(void, CSM_CODE) class_Csm_Functionality::RandomGenerate(void){
 }
 
-FUNC(void, CSM_CODE) class_Csm_Unused::KeyElementSet(void){
+FUNC(void, CSM_CODE) class_Csm_Functionality::KeyElementSet(void){
 }
 
-FUNC(void, CSM_CODE) class_Csm_Unused::KeySetValid(void){
+FUNC(void, CSM_CODE) class_Csm_Functionality::KeySetValid(void){
 }
 
-FUNC(void, CSM_CODE) class_Csm_Unused::KeySetInvalid(void){
+FUNC(void, CSM_CODE) class_Csm_Functionality::KeySetInvalid(void){
 }
 
-FUNC(void, CSM_CODE) class_Csm_Unused::KeyGetStatus(void){
+FUNC(void, CSM_CODE) class_Csm_Functionality::KeyGetStatus(void){
 }
 
-FUNC(void, CSM_CODE) class_Csm_Unused::KeyElementGet(void){
+FUNC(void, CSM_CODE) class_Csm_Functionality::KeyElementGet(void){
 }
 
-FUNC(void, CSM_CODE) class_Csm_Unused::KeyElementCopy(void){
+FUNC(void, CSM_CODE) class_Csm_Functionality::KeyElementCopy(void){
 }
 
-FUNC(void, CSM_CODE) class_Csm_Unused::KeyCopy(void){
+FUNC(void, CSM_CODE) class_Csm_Functionality::KeyCopy(void){
 }
 
-FUNC(void, CSM_CODE) class_Csm_Unused::KeyElementCopyPartial(void){
+FUNC(void, CSM_CODE) class_Csm_Functionality::KeyElementCopyPartial(void){
 }
 
-FUNC(void, CSM_CODE) class_Csm_Unused::RandomSeed(void){
+FUNC(void, CSM_CODE) class_Csm_Functionality::RandomSeed(void){
 }
 
-FUNC(void, CSM_CODE) class_Csm_Unused::KeyGenerate(void){
+FUNC(void, CSM_CODE) class_Csm_Functionality::KeyGenerate(void){
 }
 
-FUNC(void, CSM_CODE) class_Csm_Unused::KeyDerive(void){
+FUNC(void, CSM_CODE) class_Csm_Functionality::KeyDerive(void){
 }
 
-FUNC(void, CSM_CODE) class_Csm_Unused::KeyExchangeCalcPubValue(void){
+FUNC(void, CSM_CODE) class_Csm_Functionality::KeyExchangeCalcPubValue(void){
 }
 
-FUNC(void, CSM_CODE) class_Csm_Unused::KeyExchangeCalcSecret(void){
+FUNC(void, CSM_CODE) class_Csm_Functionality::KeyExchangeCalcSecret(void){
 }
 
-FUNC(void, CSM_CODE) class_Csm_Unused::JobKeySetValid(void){
+FUNC(void, CSM_CODE) class_Csm_Functionality::JobKeySetValid(void){
 }
 
-FUNC(void, CSM_CODE) class_Csm_Unused::JobKeySetInvalid(void){
+FUNC(void, CSM_CODE) class_Csm_Functionality::JobKeySetInvalid(void){
 }
 
-FUNC(void, CSM_CODE) class_Csm_Unused::JobRandomSeed(void){
+FUNC(void, CSM_CODE) class_Csm_Functionality::JobRandomSeed(void){
 }
 
-FUNC(void, CSM_CODE) class_Csm_Unused::JobKeyGenerate(void){
+FUNC(void, CSM_CODE) class_Csm_Functionality::JobKeyGenerate(void){
 }
 
-FUNC(void, CSM_CODE) class_Csm_Unused::JobKeyDerive(void){
+FUNC(void, CSM_CODE) class_Csm_Functionality::JobKeyDerive(void){
 }
 
-FUNC(void, CSM_CODE) class_Csm_Unused::JobKeyExchangeCalcPubValue(void){
+FUNC(void, CSM_CODE) class_Csm_Functionality::JobKeyExchangeCalcPubValue(void){
 }
 
-FUNC(void, CSM_CODE) class_Csm_Unused::JobKeyExchangeCalcSecret(void){
+FUNC(void, CSM_CODE) class_Csm_Functionality::JobKeyExchangeCalcSecret(void){
 }
 
-FUNC(void, CSM_CODE) class_Csm_Unused::SaveContextJob(void){
+FUNC(void, CSM_CODE) class_Csm_Functionality::SaveContextJob(void){
 }
 
-FUNC(void, CSM_CODE) class_Csm_Unused::RestoreContextJob(void){
+FUNC(void, CSM_CODE) class_Csm_Functionality::RestoreContextJob(void){
 }
 
-FUNC(void, CSM_CODE) class_Csm_Unused::CancelJob(void){
+FUNC(void, CSM_CODE) class_Csm_Functionality::CancelJob(void){
 }
 
-FUNC(void, CSM_CODE) class_Csm_Unused::CbNotification(void){
+FUNC(void, CSM_CODE) class_Csm_Functionality::CbNotification(void){
 }
 
 /******************************************************************************/
