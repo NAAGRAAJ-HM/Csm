d12e4d1957f9d12386c949d5ceca6d0b2219c5aa
Nagaraja HM <nagaraja.hm.0001@gmail.com>
2022-10-24 22:03:50 +0200

    Update: AUTOSAR basic software service module for crypto stack

--- main_LATEST/ServiceCsm.cpp
+++ main_LATEST/ServiceCsm.cpp
@@ -1,5 +1,5 @@
 /******************************************************************************/
-/* File   : ServiceCsm.cpp                                                           */
+/* File   : ServiceCsm.cpp                                                    */
 /* Author : NAGARAJA HM (c) since 1982. All rights reserved.                  */
 /******************************************************************************/
 
@@ -13,8 +13,8 @@
 /******************************************************************************/
 /* #DEFINES                                                                   */
 /******************************************************************************/
-#define SERVICECSM_AR_RELEASE_VERSION_MAJOR                                           4
-#define SERVICECSM_AR_RELEASE_VERSION_MINOR                                           3
+#define SERVICECSM_AR_RELEASE_VERSION_MAJOR                                    4
+#define SERVICECSM_AR_RELEASE_VERSION_MINOR                                    3
 
 /******************************************************************************/
 /* MACROS                                                                     */
