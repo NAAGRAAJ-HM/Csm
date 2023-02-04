78aaad15274229c3bb0aa5b64c24cc9d0f0a44cb
Nagaraja HM <nagaraja.hm.0001@gmail.com>
2023-02-04 19:29:53 +0100

    Update: AUTOSAR basic software service module for crypto stack

--- main_R2023/infServiceCsm_ServiceNvM_Types.hpp
+++ main_R2023/infServiceCsm_ServiceNvM_Types.hpp
@@ -10,9 +10,11 @@
 class NvM_BlocksRom_ServiceCsm_Type:
       public ConstModule_TypeAbstract
 {
-/******************************************************************************/
-/* CONSTS                                                                     */
-/******************************************************************************/
+};
+
+class NvM_BlocksNv_ServiceCsm_Type:
+      public CfgModule_TypeAbstract
+{
 };
 
 /******************************************************************************/
